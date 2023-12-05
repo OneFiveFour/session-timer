3e6b25db5fea934aeda0c72c83c39da4fefe27cf
Florian Meyer <florian.meyer@onefivefour.net>
2023-12-05 21:49:15 +0100

    prepare for theme extraction

--- session-editor/build.gradle.kts
+++ session-editor/build.gradle.kts
@@ -10,6 +10,7 @@ android {
 dependencies {
     implementation(platform(libs.androidX.compose.bom))
     implementation(libs.androidX.compose.ui)
+    debugImplementation(libs.androidX.compose.ui.tooling)
     implementation(libs.androidX.compose.ui.tooling.preview)
     implementation(libs.androidX.compose.material3)
 }
--- session-editor/src/main/kotlin/net/onefivefour/sessiontimer/sessioneditor/SessionEditor.kt
+++ session-editor/src/main/kotlin/net/onefivefour/sessiontimer/sessioneditor/SessionEditor.kt
@@ -1,5 +1,7 @@
 package net.onefivefour.sessiontimer.sessioneditor
 
+import android.content.res.Resources.Theme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -10,7 +12,10 @@ fun SessionEditor(
     modifier: Modifier = Modifier
 ) {
 
-    Text("TESTI")
+    Text(
+        color = MaterialTheme.colorScheme.onBackground,
+        text = "TESTI"
+    )
 
 }
 
