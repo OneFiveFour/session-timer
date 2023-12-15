8080c5edbb77f429e0facf04e6729d950d71db90
Florian Meyer <florian.fl.meyer-extern@deutschebahn.com>
2023-12-15 10:49:58 +0100

    add UnitTests for SessionRepository

---
+++ database/src/test/kotlin/net/onefivefour/sessiontimer/database/domain/SessionRepositoryTest.kt
@@ -0,0 +1,49 @@
+package net.onefivefour.sessiontimer.database.domain
+
+import com.google.common.truth.Truth.assertThat
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import net.onefivefour.sessiontimer.database.Session as DatabaseSession
+import net.onefivefour.sessiontimer.database.data.SessionDataSource
+import net.onefivefour.sessiontimer.database.domain.model.Session as DomainSession
+import org.junit.jupiter.api.Test
+
+
+class SessionRepositoryTest {
+
+    private val sessionDataSourceMock :SessionDataSource = mockk()
+
+    private val sut = SessionRepository(
+        sessionDataSourceMock
+    )
+
+    @Test
+    fun `getAll fetches data from sessionDataSource`() = runTest {
+        val testSession = DatabaseSession(78L, "Title 1")
+        every { sessionDataSourceMock.getAll() } returns flowOf(listOf(testSession))
+
+        sut.getAll().first()
+
+        coVerify(exactly = 1) { sessionDataSourceMock.getAll() }
+    }
+
+    @Test
+    fun `getAll returns domain model session`() = runTest {
+        val testSession = DatabaseSession(78L, "Title 1")
+        every { sessionDataSourceMock.getAll() } returns flowOf(listOf(testSession))
+
+        val sessions = sut.getAll().first()
+
+        assertThat(sessions.size).isEqualTo(1)
+        val session = sessions.first()
+        assertThat(session).isInstanceOf(DomainSession::class.java)
+        assertThat(session.id).isEqualTo(78L)
+        assertThat(session.title).isEqualTo("Title 1")
+        assertThat(session.taskGroups).isEmpty()
+    }
+
+}
