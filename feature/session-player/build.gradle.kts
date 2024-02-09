eadfd8181b3b0925a722044bc13d91490dad0a58
Florian Meyer <florian.fl.meyer-extern@deutschebahn.com>
2024-02-09 16:55:39 +0100

    add session player module

---
+++ feature/session-player/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    alias(libs.plugins.ksp)
+    id("st.android-library")
+    id("st.kotlin-test")
+    id("st.ktlint")
+}
+
+android {
+    namespace = AppConfig.applicationId + ".feature.sessionplayer"
+}
+
+dependencies {
+    implementation(project(":core:common"))
+    implementation(project(":core:theme"))
+    implementation(project(":core:usecases"))
+
+    // Navigation
+    api(libs.androidX.navigation)
+
+    // Compose
+    implementation(platform(libs.androidX.compose.bom))
+    implementation(libs.androidX.compose.ui)
+    debugImplementation(libs.androidX.compose.ui.tooling)
+    implementation(libs.androidX.compose.ui.tooling.preview)
+    implementation(libs.androidX.compose.material3)
+
+    // DI
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.navigation)
+    ksp(libs.hilt.android.compiler)
+
+    // collectAsStateWithLifecycle
+    implementation(libs.androidX.lifecycle.compose)
+}
