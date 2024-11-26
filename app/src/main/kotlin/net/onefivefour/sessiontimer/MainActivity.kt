5477d709749529df1c6ea68d4e4062f5c5f93e39
Florian Meyer <florian.meyer@onefivefour.net>
2024-11-26 13:33:25 +0100

    enable edgetoedge insets

--- app/src/main/kotlin/net/onefivefour/sessiontimer/MainActivity.kt
+++ app/src/main/kotlin/net/onefivefour/sessiontimer/MainActivity.kt
@@ -3,9 +3,9 @@ package net.onefivefour.sessiontimer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
 import net.onefivefour.sessiontimer.core.theme.SessionTimerTheme
@@ -15,13 +15,11 @@ import net.onefivefour.sessiontimer.navigation.AppNavGraph
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
-            SessionTimerTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+            SessionTimerTheme{
+                Box(Modifier.safeDrawingPadding()) {
                     AppNavGraph()
                 }
             }
