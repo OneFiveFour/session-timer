4eb8eabda33ca0c29b01dc3ecf24d22eccd0acb5
Florian Meyer <florian.meyer@onefivefour.net>
2024-12-08 21:37:52 +0100

    delete obsolete files after refactoring

--- feature/session-player/src/main/kotlin/net/onefivefour/sessiontimer/feature/sessionplayer/model/UiTask.kt
+++
@@ -1,9 +0,0 @@
-package net.onefivefour.sessiontimer.feature.sessionplayer.model
-
-import kotlin.time.Duration
-
-internal data class UiTask(
-    val id: Long,
-    val title: String,
-    val duration: Duration
-)
--- feature/session-player/src/main/kotlin/net/onefivefour/sessiontimer/feature/sessionplayer/model/UiTaskGroup.kt
+++
@@ -1,8 +0,0 @@
-package net.onefivefour.sessiontimer.feature.sessionplayer.model
-
-internal data class UiTaskGroup(
-    val id: Long,
-    val title: String,
-    val color: Long,
-    val tasks: List<UiTask>
-)
